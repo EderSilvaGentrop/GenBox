784fb8a676dfce980eadd82a2a4af3a811b13d9c
EderSilvaGentrop <ederson.silva@gentrop.com>
2026-05-02 16:08:03 -0300

    Atualizaçao SDK

--- components/EvergageLoader.tsx
+++ components/EvergageLoader.tsx
@@ -5,7 +5,7 @@ import Script from "next/script";
 import { useEffect, useState } from "react";
 
 const SDK_MAP: Record<string, string> = {
-  ecommerce: "https://cdn.evgnet.com/beacon/partnergentropbr/genboxecommerce/scripts/evergage.min.js",
+  ecommerce: "https://cdn.c360a.salesforce.com/beacon/c360a/9a3943f2-6525-4c02-bac2-5197e7ab74ea/scripts/c360a.min.js",
   farmacia: "https://cdn.evgnet.com/beacon/partnergentropbr/genboxfarmacia/scripts/evergage.min.js",
   financeiro: "https://cdn.evgnet.com/beacon/partnergentropbr/genboxfinanceiro/scripts/evergage.min.js",
   educacao: "https://cdn.evgnet.com/beacon/partnergentropbr/genboxeducacional/scripts/evergage.min.js",
