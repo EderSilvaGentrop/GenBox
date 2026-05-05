37ef2f16eb18c92c5f678d2ec7116bfc94dd7147
EderSilvaGentrop <ederson.silva@gentrop.com>
2026-05-05 15:03:53 -0300

    Atualização SDK Data 360

--- components/EvergageLoader.tsx
+++ components/EvergageLoader.tsx
@@ -5,10 +5,7 @@ import Script from "next/script";
 import { useEffect, useState } from "react";
 
 const SDK_MAP: Record<string, string> = {
-  ecommerce: "https://cdn.c360a.salesforce.com/beacon/c360a/6c94305f-8537-46c1-b8f3-38c07bf94d3b/scripts/c360a.min.js",
-  farmacia: "https://cdn.evgnet.com/beacon/partnergentropbr/genboxfarmacia/scripts/evergage.min.js",
-  financeiro: "https://cdn.evgnet.com/beacon/partnergentropbr/genboxfinanceiro/scripts/evergage.min.js",
-  educacao: "https://cdn.evgnet.com/beacon/partnergentropbr/genboxeducacional/scripts/evergage.min.js",
+  ecommerce: "https://cdn.c360a.salesforce.com/beacon/c360a/6c94305f-8537-46c1-b8f3-38c07bf94d3b/scripts/c360a.min.js"
 };
 
 export default function EvergageLoader() {
