0be7cf0f4c569e0fc8d2f46ffa896653b736e9cb
EderSilvaGentrop <ederson.silva@gentrop.com>
2026-05-31 19:19:51 -0300

    Layout - Global

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,22 @@
 import "./globals.css"; 
 import { CartProvider } from '../context/CartContext'; 
 import Footer from '../components/Footer'; 
-import EvergageLoader from "../components/EvergageLoader"; // Novo Loader Dinâmico
+import EvergageLoader from "../components/EvergageLoader"; 
 import CookieBanner from "../components/CookieBanner";
+import Script from "next/script"; // 1. Importa o componente de Script do Next.js
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
+      <head>
+        {/* 2. Injeta o arquivo principal do SDK com prioridade máxima */}
+        <Script 
+          src="https://sitegenbox.vercel.app/c360a.min.js" // URL do seu script baseado nos logs anteriores
+          strategy="beforeInteractive"
+        />
+      </head>
       <body>
-        {/* Agora o carregamento é dinâmico baseado na vertical ativa */}
+        {/* Mantém o Loader se ele carregar as regras de negócio/sitemap dinâmico */}
         <EvergageLoader />
 
         <CartProvider>
